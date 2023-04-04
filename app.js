73e6dbb23819acc8a432ef14bfad8c4400944402
Angvc <114889209+Angvc@users.noreply.github.com>
2023-04-04 14:27:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send('Nama: Ang Vincent Colin  NIM: 52020006  Jurusan : Teknik Informatika \n')
 })
 
 app.listen(PORT, HOST)
